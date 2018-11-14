816a749bf3e14bee4e92fce500f0fba7ff110c35
Drake-Borror <drake.borror@usd470.com>
2018-11-14 11:04:54 -0600

    Final Code
    This is our final code. (i hope this worked)

--- 1_3_9-10_sourceFiles/team10.py
+++ 1_3_9-10_sourceFiles/team10.py
@@ -6,17 +6,23 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'DaBearsGOD' # Only 10 chars displayed.
+strategy_name = 'Mitch for a TD'
+strategy_description = 'We will collude the first time and then betray every time unless they colluded'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
+    my_score, their_score are ints
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='b' and their_history[-1]=='c':
+        return 'c' # Collude if we betrayed them last time,
+    else:
+        return 'b' # otherwise betray
+
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +32,6 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
